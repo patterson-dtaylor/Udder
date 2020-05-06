6108fafd758ac255c01a7af71d7fef4eaa96560a
patterson-dtaylor <patterson.dtaylor@gmail.com>
2020-05-06 19:39:57 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Taylor Patterson on 5/6/20.
+//  Copyright © 2020 Taylor Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
